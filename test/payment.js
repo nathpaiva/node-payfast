cbaf0a8e6d3b0068773f1fd3cb479f22fdbb3475
NathPaiva <hello@nathpaiva.com.br>
2017-06-15 07:40:32 -0300

    test confirm payment

--- test/payment.js
+++ test/payment.js
@@ -1,48 +1,3 @@
-// var express = require('../config/custom-express')();
-// var request = require('supertest')(express);
-
-// describe('#Payment Controller', function () {
-
-//   // beforeEach(function (done) {
-//   //   var conn = express.infra.connectionFactory();
-//   //   conn.query('delete from payment', function (ex, result) {
-//   //     if (!ex) {
-//   //       done();
-//   //     }
-//   //   });
-//   // });
-//   beforeEach(function () {
-//     express.infra.connectionFactory();
-//     // conn.query('delete from livros', function (ex, result) {
-//     //   if (!ex) {
-//     //     done();
-//     //   }
-//     // });
-//   });
-
-//   it('#List payments', function () {
-// request.get('/payments/payment')
-//       .set('Accept', 'application/json')
-//       .expect('Content-type', 'application/json')
-//       .expect(200);
-//   });
-
-//   it('#Create new payment', function () {
-//     request.post('/payments/pajijioyment')
-//       .send({
-//         forma_de_pagamento: 'payfast',
-//         valor: '20.87',
-//         moeda: 'BRL',
-//         descricao: 'descrição do pagamentinho'
-//       })
-//       .set('Accept', 'application/json')
-//       .expect('Content-type', /json/)
-//       .expect(201);
-//   });
-
-// });
-
-
 var express = require('../config/express')();
 var request = require('supertest')(express);
 
@@ -78,5 +33,29 @@ describe('#Produtos Controller', function () {
       .expect('Content-type', /json/)
       .expect(201, done);
   });
+
+  it('#Create new payment to confirm', function (done) {
+    const payment_create = {
+      forma_de_pagamento: 'payfast',
+      valor: '20.87',
+      moeda: 'BRL',
+      descricao: 'descrição do pagamentinho'
+    };
+
+    request.post('/payments/payment')
+      .send(payment_create)
+      .set('Accept', 'application/json')
+      .then(response => {
+        confimPayment(response.body.data.id);
+      });
+
+    const confimPayment = id => {
+      request.put(`/payments/payment/${id}`)
+        .set('Accept', 'application/json')
+        .expect('Content-type', /json/)
+        .expect(200, done);
+    }
+  });
+
 
 });
